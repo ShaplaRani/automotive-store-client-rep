de879d016aa77706264e87b76560ebbe10d3426a
ShaplaRani <shaplarani621@gmail.com>
2023-10-20 00:10:15 -0700

    add the slider in product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -25,13 +25,56 @@ const Product = () => {
     } )
    },[brand_name])
    
-   console.log(uniqueProducts);
+   console.log(uniqueProducts.length);
     return (
-        <div className="w-11/12 lg:w-10/12 m-auto">
+        <div className="w-11/12 lg:w-10/12 m-auto mt-14">
+
+       
+<div className="carousel w-full h-[70vh]">
+  <div id="slide1" className="carousel-item relative w-full h-[70vh]">
+    <img src="https://i.ibb.co/D8TzZZY/IMG-20231019-WA0014.jpg" className="w-full h-[70vh]" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="https://i.ibb.co/424Jky2/IMG-20231019-WA0017.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="https://i.ibb.co/qsB7N8J/IMG-20231019-WA0015.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="https://i.ibb.co/pQz7FJx/IMG-20231019-WA0024.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+
+
+         <h2 className=" mt-16 mb-8 text-3xl font-semibold text-center">
+             Product of <span className="text-orange-600">{brand_name}</span> brand
+         </h2>
+
             
-           <div className="grid grid-cols-1 lg:grid-cols-2 gap-7 my-20">
+           <div className="grid grid-cols-1 lg:grid-cols-2 gap-7 mb-20 ">
             {
-                 uniqueProducts.map(product => <ProductCard key={product._id} product={product}></ProductCard> )
+                uniqueProducts.length > 0 ?
+                 uniqueProducts?.map(product =>
+                     <ProductCard key={product._id} product={product}></ProductCard> ): 
+                     <p className=" text-blue-800 my-40 items-center grid col-span-2 text-4xl font-extrabold text-center">No Data Found
+                      <img src="" alt="" />
+                     </p>
             }
            </div>
         </div>
